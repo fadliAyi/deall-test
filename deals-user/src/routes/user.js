915a1d91c0bdc1b63c1b8c50570bfa90c3ec0282
fadli <m.fadli4545@gmail.com>
2023-05-16 00:50:07 +0800

    fix middleware

--- deals-user/src/routes/user.js
+++ deals-user/src/routes/user.js
@@ -4,9 +4,9 @@ const isRoleAdmin = require('../middleware/isRoleAdmin');
 const isRoleUser = require('../middleware/isRoleUser');
 const userController = require('../controller/userController');
 
-router.post('/', userController.create);
+router.post('/', [authMiddleware, isRoleAdmin], userController.create);
 router.get('/:id', [authMiddleware], userController.detail);
-router.get('/', userController.list);
+router.get('/', [authMiddleware, isRoleAdmin], userController.list);
 router.put('/:id', [authMiddleware, isRoleAdmin], userController.update);
 router.delete('/:id', [authMiddleware, isRoleAdmin], userController.destroy);
 
